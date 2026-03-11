b2ab7557ec285f9b958f61ad2976803b1a7875f3
john lemon <jojo.john.jojo.jojo@gmail.com>
2026-03-12 08:13:59 +0900

    add map preview with leaflet

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 console.log("My Map Service LP loaded");
+
+const links = document.querySelectorAll('a[href^="#"]');
+
+links.forEach((link) => {
+    link.addEventListener("click", (event) => {
+        event.preventDefault();
+
+        const targetId = link.getAttribute("href");
+        const target = document.querySelector(targetId);
+
+        if (target) {
+            target.scrollIntoView({
+                behavior: "smooth",
+                block: "start",
+            });
+        }
+    });
+});
+const map = L.map('map').setView([35.681236, 139.767125], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; OpenStreetMap'
+}).addTo(map);
+
+const marker = L.marker([35.681236, 139.767125]).addTo(map);
+
+marker.bindPopup("Tokyo Station").openPopup();
